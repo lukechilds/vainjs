24373876ac9feb802bf7c69ca2c8799ed1046f95
Luke Childs <lukechilds123@gmail.com>
2019-05-08 16:42:18 +0700

    Add basic P2PKH address miner

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-const vanity = () => {};
+const bitcoin = require('bitcoinjs-lib');
 
-module.exports = vanity;
+class Vain {
+	constructor({prefix}) {
+		this.prefix = `1${prefix}`;
+	}
+
+	start() {
+		const startTime = Date.now();
+
+		let found;
+		let attempts = 0;
+		let keyPair;
+		let address;
+
+		while (!found) {
+			attempts++;
+			keyPair = bitcoin.ECPair.makeRandom();
+			({address} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey}));
+
+			if (address.startsWith(this.prefix)) {
+				found = true;
+			}
+		}
+
+		const endTime = Date.now();
+		const duration = endTime - startTime;
+		const trysPerSecond = Math.floor(attempts / (duration / 1000));
+
+		return {
+			duration,
+			trysPerSecond,
+			address,
+			wif: keyPair.toWIF()
+		};
+	}
+};
+
+module.exports = Vain;
