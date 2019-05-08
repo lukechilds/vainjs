9f5e995e2d8a06e9a107edaaf95c36570fe81c8d
Luke Childs <lukechilds123@gmail.com>
2019-05-09 05:29:33 +0700

    Emit progress event once a second

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const Emitter = require('tiny-emitter');
 const bitcoin = require('bitcoinjs-lib');
 
-class Vain {
+class Vain extends Emitter {
 	constructor({prefix}) {
+		super();
 		this.prefix = `1${prefix}`;
 	}
 
@@ -12,6 +14,7 @@ class Vain {
 		let attempts = 0;
 		let keyPair;
 		let address;
+		let lastUpdate = Date.now();
 
 		while (!found) {
 			attempts++;
@@ -20,6 +23,18 @@ class Vain {
 
 			if (address.startsWith(this.prefix)) {
 				found = true;
+			}
+
+			const now = Date.now();
+			if((now - lastUpdate) > 1000) {
+				const duration = now - startTime;
+				const trysPerSecond = Math.floor(attempts / (duration / 1000));
+				this.emit('update', {
+					duration,
+					attempts,
+					trysPerSecond,
+				});
+				lastUpdate = now;
 			}
 		}
 
