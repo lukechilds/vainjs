a92f7aea27756839ea9c6fc598e3733d75c2fed7
Luke Childs <lukechilds123@gmail.com>
2019-05-09 08:23:53 +0700

    Don't print benchmark progress updates in CI

--- bench/index.js
+++ bench/index.js
@@ -1,6 +1,8 @@
 const Vain = require('..');
 const prettyMs = require('pretty-ms');
 
+const isCI = process.env.CI;
+
 const options = [
 	{
 		addressFormat: 'p2pkh',
@@ -24,12 +26,14 @@ options.forEach(options => {
 
 	const vain = new Vain(options);
 
-	vain.on('update', data => {
-		const duration = prettyMs(data.duration);
-		const {attempts} = data;
-		const speed = `${data.addressesPerSecond} addr/s`;
-		console.log(`Duration: ${duration} | Attempts: ${attempts} | Speed: ${speed}`);
-	});
+  if (!isCI) {
+    vain.on('update', data => {
+      const duration = prettyMs(data.duration);
+      const {attempts} = data;
+      const speed = `${data.addressesPerSecond} addr/s`;
+      console.log(`Duration: ${duration} | Attempts: ${attempts} | Speed: ${speed}`);
+    });
+  }
 
 	vain.on('found', data => {
 		console.log();
