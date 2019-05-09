d23a138de4f1eab86d0504136c5e736131ea34cc
Luke Childs <lukechilds123@gmail.com>
2019-05-09 07:55:16 +0700

    Test p2wpkh-p2sh address

--- test/unit.js
+++ test/unit.js
@@ -23,3 +23,20 @@ test('Vain derives a p2pkh vanity address', async t => {
 	t.true(address.startsWith(`1${options.prefix}`));
 	t.is(address, wifAddress);
 });
+
+test('Vain derives a p2wpkh-p2sh vanity address', async t => {
+	const options = {
+		addressFormat: 'p2wpkh-p2sh',
+		prefix: 'A'
+	};
+	const vain = new Vain(options);
+	const {address, wif} = await vain.start();
+
+	const keyPair = bitcoin.ECPair.fromWIF(wif);
+	const {address: wifAddress} = bitcoin.payments.p2sh({
+		redeem: bitcoin.payments.p2wpkh({pubkey: keyPair.publicKey})
+	});
+
+	t.true(address.startsWith(`3${options.prefix}`));
+	t.is(address, wifAddress);
+});
