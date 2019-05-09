da3fd6453b287e592af413cfe1fa230580d4731c
Luke Childs <lukechilds123@gmail.com>
2019-05-09 07:53:59 +0700

    Test p2pkh address

--- test/unit.js
+++ test/unit.js
@@ -1,6 +1,25 @@
 import test from 'ava';
+// For some reason AVA can't `import` bitcoin so `require` for now.
+// import bitcoin from 'bitcoinjs-lib';
 import Vain from '..';
+
+const bitcoin = require('bitcoinjs-lib');
 
 test('Vain is exported', t => {
 	t.not(Vain, undefined);
+});
+
+test('Vain derives a p2pkh vanity address', async t => {
+	const options = {
+		addressFormat: 'p2pkh',
+		prefix: 'A'
+	};
+	const vain = new Vain(options);
+	const {address, wif} = await vain.start();
+
+	const keyPair = bitcoin.ECPair.fromWIF(wif);
+	const {address: wifAddress} = bitcoin.payments.p2pkh({pubkey: keyPair.publicKey});
+
+	t.true(address.startsWith(`1${options.prefix}`));
+	t.is(address, wifAddress);
 });
