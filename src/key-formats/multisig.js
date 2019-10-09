69882f82936dd285b3391eba4b40240f94acc97d
Luke Childs <lukechilds123@gmail.com>
2019-10-09 10:10:39 +0700

    Enforce BIP 67 lexicographically ordered pubkeys for multisig

--- src/key-formats/multisig.js
+++ src/key-formats/multisig.js
@@ -3,13 +3,17 @@ const bitcoin = require('bitcoinjs-lib');
 const generateRedeemScript = ({pubkeys, m}) => {
 	const keyPair = bitcoin.ECPair.makeRandom();
 	const {publicKey} = keyPair;
+
+	// BIP 67 lexicographically ordered pubkeys
+	pubkeys = [
+		...pubkeys,
+		publicKey
+	].sort(Buffer.comapre);
+
 	let redeemScript;
 	let n;
 	({output: redeemScript, m, n, pubkeys} = bitcoin.payments.p2ms({
-		pubkeys: [
-			...pubkeys,
-			publicKey
-		],
+		pubkeys,
 		m
 	}));
 
