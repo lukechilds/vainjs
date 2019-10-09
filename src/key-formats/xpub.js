76348d8b0526c26aa78edec8230938c786d0a082
Luke Childs <lukechilds123@gmail.com>
2019-10-09 11:09:05 +0700

    TODO: Cache the derived node for quick perf gain

--- src/key-formats/xpub.js
+++ src/key-formats/xpub.js
@@ -3,6 +3,7 @@ const bitcoin = require('bitcoinjs-lib');
 const generateXpubKey = ({attempts, xpub}) => {
 	const index = attempts - 1;
 
+	// TODO: Cache the derived node for quick perf gain
 	const node = bitcoin.bip32.fromBase58(xpub);
 	const {publicKey} = node.derive(index);
 
